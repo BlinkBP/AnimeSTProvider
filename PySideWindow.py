657d7f1a41062bb729ef1e317e81b3407d28a126
BlinkBP <jan.kominczyk@gmail.com>
2017-02-22 09:23:02 +0000

    Variable names changes in PySideWindow.py
    Deleted unnecessary variables

--- PySideWindow.py
+++ PySideWindow.py
@@ -17,55 +17,50 @@ class Window(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        # # Menu
-        # self.actionOpen_HTML.triggered.connect(self.open_HTML)
-        # self.actionAbout.triggered.connect(self.show_about)
-        # self.actionLoad_JSON_auth_file.triggered.connect(self.log_in)
-        # # Buttons
-        # self.closeBtn = QPushButton()
-        # self.tab1deleteAnimeBtn.clicked.connect(self.delete_anime)
-        # self.tab1addAnimeBtn.clicked.connect(self.add_anime)
-        # self.tab1searchYTBtn.clicked.connect(self.search)
-        # self.tab0addPlaylistBtn.clicked.connect(self.create_playlist)
-        # self.tab1addPlaylistBtn.clicked.connect(self.create_playlist)
-        # self.tab0deletePlaylistBtn.clicked.connect(self.delete_playlist)
-        # self.tab0loadPlaylistBtn.clicked.connect(self.load_playlist)
-        # self.tab0nextPageBtn.clicked.connect(self.nextPage)
-        # self.tab1nextPageBtn.clicked.connect(self.nextPage)
-        # self.tab0previousPageBtn.clicked.connect(self.previousPage)
-        # self.tab1previousPageBtn.clicked.connect(self.previousPage)
-        # self.closeBtn.clicked.connect(self.destroy_preview)
-        # #Variables
-        # self.playlistIds = []
-        # self.playlistVideoUrlIds = []
-        # self.playlistVideoIds = []
-        # self.playlistVideoTitles = []
-        # self.playlistVideoThumbnails = []
-        # self.videoUrlIds = []
-        # self.videoIds = []
-        # self.videoTitles = []
-        # self.videoThumbnails = []
-        # self.currentPlaylistRow = 0
-        # self.currentAnimeRow = 0
-        # self.totalVideoPages = 0
-        # self.totalPlaylistPages = 0
-        # self.currentVideoPage = 0
-        # self.currentPlaylistPage = 0
-        # #Events
-        # self.tab0userPlaylistsList.itemClicked.connect(self.on_playlist_row_changed)
-        # self.tab1userPlaylistsList.itemClicked.connect(self.on_playlist_row_changed)
-        # self.tab1animesList.itemClicked.connect(self.on_anime_row_changed)
-        # #Icons
-        # self.icon = QIcon("assets/icon.png")
-        # #Misc
-        # self.previewWindow = QWidget()
-        # self.webView = QWebView()
-        #
-        # self.setWindowIcon(self.icon)
+        self.connectButtons()
+        self.connectMenus()
+        self.connectEvents()
+        self.setWindowIcon(QIcon("assets/icon.png"))
+        self.loggedIn = False
+        
+    def connectButtons(self):
+        self.closeBtn = QPushButton()
+        self.tab1DeleteAnimeBtn.clicked.connect(self.delete_anime)
+        self.tab1AddAnimeBtn.clicked.connect(self.add_anime)
+        self.tab1SearchYTBtn.clicked.connect(self.search)
+        self.tab0AddPlaylistBtn.clicked.connect(self.create_playlist)
+        self.tab1AddPlaylistBtn.clicked.connect(self.create_playlist)
+        self.tab0DeletePlaylistBtn.clicked.connect(self.delete_playlist)
+        self.tab0LoadPlaylistBtn.clicked.connect(self.load_playlist)
+        self.closeBtn.clicked.connect(self.destroy_preview)
+
+    def connectMenus(self):
+        self.actionOpen_HTML.triggered.connect(self.open_HTML)
+        self.actionAbout.triggered.connect(self.show_about)
+        self.actionLoad_JSON_auth_file.triggered.connect(self.log_in)
+
+    def connectEvents(self):
+        self.tab0UserPlaylistsList.itemClicked.connect(
+            self.on_playlist_row_changed)
+        self.tab1UserPlaylistsList.itemClicked.connect(
+            self.on_playlist_row_changed)
+        self.tab1AnimesList.itemClicked.connect(
+            self.on_anime_row_changed)
+
+    def load_playlist(self):
+        if (self.loggedIn):
+            self.playlistVideoTitles, self.playlistVideoThumbnails, self.playlistVideoIds, self.playlistVideoUrlIds	= YouTubeProvider.load_playlist(
+                self.playlistIds[self.currentPlaylistRow]
+                )
+            self.fill_thumbnails(self.playlistVideoThumbnails, "playlist")
+            self.fill_labels(self.playlistVideoTitles, "playlist")
+            self.update_page_label("playlist")
+            self.statusBar().showMessage('Playlist loaded!')
+        else:
+            self.showError("You must log in first!")
 
     def initialize_preview(self):
         self.previewWindow.setWindowFlags(Qt.FramelessWindowHint)
-        #self.previewWindow.setFixedSize(685,470)
         self.previewWindow.setWindowTitle("Preview")
         self.layout = QVBoxLayout()
         self.previewWindow.setLayout(self.layout)
@@ -273,21 +268,6 @@ class Window(QMainWindow, Ui_MainWindow):
             self.totalVideoPages = round(len(videos) / 6)
         elif tab == "playlist":
             self.totalPlaylistPages = round(len(videos) / 6)
-
-    def load_playlist(self):
-        if len(self.playlistIds) > 0:
-            self.playlistVideoTitles, self.playlistVideoThumbnails, self.playlistVideoIds, self.playlistVideoUrlIds	= YouTubeProvider.load_playlist(
-                self.playlistIds[self.currentPlaylistRow]
-                )
-
-            self.count_pages(self.playlistVideoTitles, "playlist")
-
-            self.fill_thumbnails(self.playlistVideoThumbnails, "playlist")
-            self.fill_labels(self.playlistVideoTitles, "playlist")
-            self.update_page_label("playlist")
-            self.statusBar().showMessage('Playlist loaded!')
-        else:
-            self.showError("You must log in first or this playlist has no videos.")
 
     def search(self):
 
